bb7d58a40ecae1cd3faca7080628064878ffab94
Erlend Wiig <erlend.wiig@gmail.com>
2013-09-02 15:20:51 +0200

    finished adding deploy matrix

--- environments.js
+++ environments.js
@@ -1,12 +1,55 @@
 var Ploy = {
-	onClicked: function (button) {
+	config: {},
+	environment: '',
+
+	onSelectedEnvironment: function (button) {
 		console.log('button: ' + button.innerText + ' clicked');
+
+		environment = button.innerText;
+		Ploy.removeButtons();
+		Ploy.replaceText($('h1'), 'Deploy Matrix for: ' + environment);
+		Ploy.addDeployMatrix();
+	},
+
+	removeButtons: function () {
+		console.log('remove all buttons');
+		$(':button').remove();
+	},
+
+	replaceText: function (element, text) {
+		element.text(text);
+	},
+
+	addDeployMatrix: function () {
+		$.ajax({
+			type: 'get',
+	    	url: '/getVersions',
+	    	success: function (versions) {
+	        	console.log('success!! addDeployMatrix');
+	        	Ploy.addVersionsElements(versions, Ploy.config.services);
+	    	}
+		});
 	},
 
 	addClickListener: function (callback) {
 		$(':button').click(function () {
 			callback(this);
 		});
+	},
+
+	addVersionsElements: function (versions, services) {
+		var body;
+
+		body = $('body');
+
+		for (var i = services.length - 1; i >= 0; i--) {
+			var appended = $('<div class="column"><p class="serviceHeader">' + services[i].id + '</p></div>').appendTo(body);
+			
+			for (var j = versions.length - 1; j >= 0; j--) {				
+				appended.append('<div class="checkbox"><input type="radio" value="' + versions[j] + '" id="roundedOne" name="service_' + services[i].id + '" >' + versions[j] +'</input></div>');
+				
+			};
+		};
 	},
 
 	addButtons: function (configuredEnvironments) {
@@ -25,10 +68,11 @@ $(document).ready(function () {
     	type: 'get',
     	url: '/getConfig',
     	success: function (data) {
-        	console.log('success!!');
+        	console.log('success!! getConfig');
 
+        	Ploy.config = data;
         	Ploy.addButtons(data.environments);	
-    		Ploy.addClickListener(Ploy.onClicked);
+    		Ploy.addClickListener(Ploy.onSelectedEnvironment);
     	}
 	});
 });
--- main.js
+++ main.js
@@ -15,6 +15,8 @@
 // }
 //     connect.static("c:\\source\\Ploy")
 // ).listen(4242);
+
+
 var config;
 var connect = require('connect');
 var fs = require('fs');
@@ -24,29 +26,64 @@ var app = connect()
   //.use(connect.logger('dev'))
   .use(connect.static('c:\\source\\Ploy'))
   .use(function(req, response){
+
   	  var url_parts = url.parse(req.url, true);
+
+
+      console.log('Request received: '+ url_parts.pathname + ' ' + url_parts.query.search);
+
       if (req.method == 'GET') {
         switch (url_parts.pathname) {
             case '/getConfig':
             	config = ReadConfig();
-            	var stringified = JSON.stringify(config);
-            	console.log(stringified);
-            	response.writeHead(200, { 'Content-Type': 'application/json' });   
-            	response.write(stringified);
-            	response.end();
-            break;
+              Respond(response, config);              
+              break;
+          
+            case '/getVersions':  
+              config = ReadConfig();
+              var versions = ReadVersions(config.versionsLocation);  
+              Respond(response, versions);
+              break;
           }
      	}
 
-  	console.log('Request received: '+ req);
-    
-    response.end('hello world\n');
+      // if (req.method == 'POST') {
+      //   switch (url_parts.pathname) {
+      //     case '/getVersions':
+      //         req.on('data', function (dataChunk) {
+      //           console.log('received data: ' + dataChunk.toString());
+      //           var parsed = JSON.parse(dataChunk);
+      //           var versions = ReadVersions(ReadConfig().versionsLocation, parsed.environment);  
+      //           Respond(response, versions);
+      //         });
+
+      //         req.on('end', function() {
+      //           console.log('request end');
+      //           response.writeHead(200, "OK", {'Content-Type': 'text/html'});
+      //           response.end();
+      //         });
+      //         break;
+      //   }
+      // }
 
   })
  .listen(4242);
 
- function ReadConfig () {
- 	config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+function ReadConfig () {
+ 	var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
  	console.log(config);
  	return config;
+ };
+
+function ReadVersions (versionsLocation) {
+  console.log('reading versions: ' + versionsLocation);
+  return fs.readdirSync(versionsLocation);
+};
+
+ function Respond (response, responseData) {
+    var stringifiedResponse = JSON.stringify(responseData);
+    console.log(stringifiedResponse);
+    response.writeHead(200, { 'Content-Type': 'application/json' });   
+    response.write(stringifiedResponse);
+    response.end();
  }
